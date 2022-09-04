098bf550a3406e7362e855ce00ac6d40fa2f3d58
Pramoth <pramoth@geniustree.co.th>
2022-09-04 21:52:05 +0700

    finish ch14 clox

---
+++ debug.h
@@ -0,0 +1,14 @@
+//
+// Created by pramoth on 9/4/22.
+//
+
+#ifndef CLOX_DEBUG_H
+#define CLOX_DEBUG_H
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk *chunk, const char *name);
+
+int disassembleInstruction(Chunk *chunk, int offset);
+
+#endif //CLOX_DEBUG_H
